c36f80f6dc0d02816516beb58cccd232d3c435f1
Ayesh Chamodye <ayeshchamodye@gmail.com>
2026-07-16 19:47:36 +0530

    style(config): update quote style in firebase configuration

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCuWpBXg9o0VCst7mADZowKro5T3pt47CA',
-  authDomain: 'sellany-502609.firebaseapp.com',
-  projectId: 'sellany-502609',
-  storageBucket: 'sellany-502609.firebasestorage.app',
-  messagingSenderId: '1098367987126',
-  appId: '1:1098367987126:web:d2c460be73f42c139fa517',
-  measurementId: 'G-6XDPLV39JN',
+  apiKey: "AIzaSyCuWpBXg9o0VCst7mADZowKro5T3pt47CA",
+  authDomain: "sellany-502609.firebaseapp.com",
+  projectId: "sellany-502609",
+  storageBucket: "sellany-502609.firebasestorage.app",
+  messagingSenderId: "1098367987126",
+  appId: "1:1098367987126:web:d2c460be73f42c139fa517",
+  measurementId: "G-6XDPLV39JN"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
